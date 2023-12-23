3c3f0eef69c7ff51c2af464c0ddbafd0d91cd2c9
josericardopenase <josericardopenase@gmail.com>
2023-12-23 15:44:14 +0000

    Created main function with dependencies

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import org.example.core.movies.application.useCases.MovieStatisticCreator;
+import org.example.core.movies.infraestructure.loaders.MockMovieLoader;
+import org.example.core.movies.infraestructure.statisticProcessors.HistogramMovieStatisticProcessor;
+
 public class Main {
     public static void main(String[] args) {
+        var loader = new MockMovieLoader();
+        var processor = new HistogramMovieStatisticProcessor();
+        var statisticCreator = new MovieStatisticCreator<>(loader, processor);
+        var statistic = statisticCreator.create();
+        System.out.println(statistic.data());
     }
 }
