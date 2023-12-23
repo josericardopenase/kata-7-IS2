01effc863e25d6196a2eb5127dc23fb8af89bbdd
josericardopenase <josericardopenase@gmail.com>
2023-12-23 15:10:53 +0000

    first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,7 @@
 package org.example;
 
-import org.example.core.movies.application.useCases.MovieStatisticCreator;
-import org.example.core.movies.domain.Movie;
-import org.example.core.movies.infraestructure.loaders.CsvFileMovieLoader;
-import org.example.core.movies.infraestructure.statisticProcessors.HistogramMovieStatisticProcessor;
-import static java.util.stream.Collectors.averagingDouble;
-
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hello world");
     }
 }
