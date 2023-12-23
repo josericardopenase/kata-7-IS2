7c46f88bcb3b4214e2773493f90b4009f20d5df0
josericardopenase <josericardopenase@gmail.com>
2023-12-23 15:10:30 +0000

    started again

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,17 +2,11 @@ package org.example;
 
 import org.example.core.movies.application.useCases.MovieStatisticCreator;
 import org.example.core.movies.domain.Movie;
-import org.example.core.movies.infraestructure.loaders.MockMovieLoader;
+import org.example.core.movies.infraestructure.loaders.CsvFileMovieLoader;
 import org.example.core.movies.infraestructure.statisticProcessors.HistogramMovieStatisticProcessor;
-
-import static java.util.stream.Collectors.averagingInt;
+import static java.util.stream.Collectors.averagingDouble;
 
 public class Main {
     public static void main(String[] args) {
-        var loader = new MockMovieLoader();
-        var processor = new HistogramMovieStatisticProcessor(Movie::genre, averagingInt(Movie::audienceScore));
-        var statisticCreator = new MovieStatisticCreator<>(loader, processor);
-        var statistic = statisticCreator.create();
-        System.out.println(statistic.data());
     }
 }
