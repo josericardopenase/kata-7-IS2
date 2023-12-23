cdc016cb1d58cb4b5b164b69b3b245cc56ff51ed
josericardopenase <josericardopenase@gmail.com>
2023-12-23 15:48:14 +0000

    Created Histogram Statistic Processor for movies

--- src/main/java/org/example/core/movies/infraestructure/statisticProcessors/HistogramMovieStatisticProcessor.java
+++ src/main/java/org/example/core/movies/infraestructure/statisticProcessors/HistogramMovieStatisticProcessor.java
@@ -6,10 +6,23 @@ import org.example.core.movies.domain.Statistic;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class HistogramMovieStatisticProcessor implements MovieStatisticProcessor<Map<String, Double>> {
+
+
+    private final Function<Movie, String> key;
+    private final Collector<Movie, ?, Double> collector;
+
+    public <T> HistogramMovieStatisticProcessor(Function<Movie, String> key, Collector<Movie,?, Double> collector) {
+        this.key = key;
+        this.collector = collector;
+    }
+
     @Override
     public Statistic<Map<String, Double>> process(List<Movie> movie) {
-        return null;
+        return new Statistic<>(movie.stream().collect(Collectors.groupingBy(key, collector)));
     }
 }
