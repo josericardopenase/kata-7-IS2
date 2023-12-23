25f6dc06339060a6bf123698222eaa7c7e4f2374
josericardopenase <josericardopenase@gmail.com>
2023-12-23 15:42:13 +0000

    Created application layer

---
+++ src/main/java/org/example/core/movies/application/useCases/MovieStatisticCreator.java
@@ -0,0 +1,24 @@
+package org.example.core.movies.application.useCases;
+
+import org.example.core.movies.application.ports.MovieLoader;
+import org.example.core.movies.application.ports.MovieStatisticProcessor;
+import org.example.core.movies.domain.Movie;
+import org.example.core.movies.domain.Statistic;
+
+import java.util.List;
+
+public class MovieStatisticCreator<T> {
+
+    private final MovieLoader loader;
+    private final MovieStatisticProcessor<T> processor;
+
+    public MovieStatisticCreator(MovieLoader loader, MovieStatisticProcessor<T> processor) {
+        this.loader = loader;
+        this.processor = processor;
+    }
+
+    public Statistic<T> create(){
+         var movies = this.loader.load();
+         return this.processor.process(movies);
+    }
+}
