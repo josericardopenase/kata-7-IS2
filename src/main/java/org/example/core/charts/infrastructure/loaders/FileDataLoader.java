e00d512c9db039c500f8cb6862619aacc7c72938
josericardopenase <josericardopenase@gmail.com>
2023-12-24 16:35:28 +0000

    Created CsvFileLoader in charts

---
+++ src/main/java/org/example/core/charts/infrastructure/loaders/FileDataLoader.java
@@ -0,0 +1,34 @@
+package org.example.core.charts.infrastructure.loaders;
+
+import org.example.core.charts.application.ports.DataLoader;
+import org.example.core.charts.application.ports.ModelSerializer;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileDataLoader<Model> implements DataLoader<Model> {
+    private final String path;
+    private final ModelSerializer<Model> serializer;
+
+    public FileDataLoader(String path, ModelSerializer<Model> serializer) {
+        this.path = path;
+        this.serializer = serializer;
+    }
+
+    @Override
+    public List<Model> load() {
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+            return load(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<Model> load(BufferedReader reader) {
+        return reader.lines()
+                .skip(1)
+                .map(serializer::serialize)
+                .collect(Collectors.toList());
+    }
+}
