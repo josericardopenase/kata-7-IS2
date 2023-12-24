17d30cde31d907d4bd6dc19e69d5c040ed4a7cf5
josericardopenase <josericardopenase@gmail.com>
2023-12-24 14:01:06 +0000

    Finished ChartCreator useCase

--- src/main/java/org/example/core/shared/application/useCases/ChartCreator.java
+++ src/main/java/org/example/core/shared/application/useCases/ChartCreator.java
@@ -1,4 +1,21 @@
 package org.example.core.shared.application.useCases;
 
-public class ChartCreator {
+import org.example.core.shared.application.ports.ChartProcessor;
+import org.example.core.shared.application.ports.DataLoader;
+import org.example.core.shared.domain.Chart;
+
+public class ChartCreator<Model, Data> {
+    private final ChartProcessor<Model, Data> processor;
+    private final DataLoader<Model> loader;
+
+
+    public ChartCreator(ChartProcessor<Model, Data> processor, DataLoader<Model> loader) {
+        this.processor = processor;
+        this.loader = loader;
+    }
+
+    public Chart<Data> create(){
+        var data = this.loader.load();
+        return this.processor.process(data);
+    }
 }
