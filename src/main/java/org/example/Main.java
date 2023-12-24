f155e380f9e4a858dce2b159d45384b47cf67fbc
josericardopenase <josericardopenase@gmail.com>
2023-12-24 16:40:32 +0000

    Created main function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,20 @@
 package org.example;
 
+import org.example.core.charts.application.useCases.ChartCreator;
+import org.example.core.charts.infrastructure.loaders.FileDataLoader;
+import org.example.core.charts.infrastructure.processors.BarChartProcessor;
+import org.example.core.movies.domain.Movie;
+import org.example.core.movies.infrastructure.CsvMovieSerializer;
+
+import static java.util.stream.Collectors.averagingInt;
+
 public class Main {
     public static void main(String[] args) {
+        var serializer = new CsvMovieSerializer();
+        var loader = new FileDataLoader<>("movies.csv", serializer);
+        var processor = new BarChartProcessor<>(Movie::genre, averagingInt(Movie::audienceScore));
+        var chartCreator = new ChartCreator<>(loader, processor);
+        var chart = chartCreator.create();
+        System.out.println(chart.data());
     }
 }
