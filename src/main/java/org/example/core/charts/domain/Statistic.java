d9bd99db20552e19678aef813dd5951389afbc68
josericardopenase <josericardopenase@gmail.com>
2024-01-20 18:28:58 +0100

    add factory method to statistic

--- src/main/java/org/example/core/charts/domain/Statistic.java
+++ src/main/java/org/example/core/charts/domain/Statistic.java
@@ -3,4 +3,7 @@ package org.example.core.charts.domain;
 public record Statistic<Data>(
         Data data
 ) {
+    public static <Data>Statistic of(Data data){
+        return new Statistic<Data>(data);
+    }
 }
