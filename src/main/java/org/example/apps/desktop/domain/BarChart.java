6606f39bd2d5224deff8e42bf1902f397c3323aa
josericardopenase <josericardopenase@gmail.com>
2024-01-20 05:33:20 +0100

    Created Domain Layer with BarChart and BarChartBuilder

---
+++ src/main/java/org/example/apps/desktop/domain/BarChart.java
@@ -0,0 +1,51 @@
+package org.example.apps.desktop.domain;
+
+import java.util.Map;
+
+public record BarChart(
+        String title,
+        String xAxisTitle,
+        String yAxisTitle,
+        Map<String, Double> data
+) {
+    public static BarChartBuilder fromScratch() {
+        return new BarChartBuilder();
+    }
+
+    public static class BarChartBuilder{
+        String title;
+        String xAxisTitle;
+        String yAxisTitle;
+
+        public BarChartBuilder setTitle(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public BarChartBuilder setxAxisTitle(String xAxisTitle) {
+            this.xAxisTitle = xAxisTitle;
+            return this;
+        }
+
+        public BarChartBuilder setyAxisTitle(String yAxisTitle) {
+            this.yAxisTitle = yAxisTitle;
+            return this;
+        }
+
+        public BarChartBuilder setData(Map<String, Double> data) {
+            this.data = data;
+            return this;
+        }
+
+        Map<String, Double> data;
+
+        public BarChart build(){
+            return new BarChart(
+                    this.title,
+                    this.xAxisTitle,
+                    this.yAxisTitle,
+                    this.data
+            );
+        }
+    }
+}
