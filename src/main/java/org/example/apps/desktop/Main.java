ef410d6ed0e284a1409b38d5827c2f0c961a855f
josericardopenase <josericardopenase@gmail.com>
2024-01-20 04:58:44 +0100

    Implemented getChartCreator method in Main of desktop app

--- src/main/java/org/example/apps/desktop/Main.java
+++ src/main/java/org/example/apps/desktop/Main.java
@@ -1,4 +1,24 @@
 package org.example.apps.desktop;
 
+import org.example.core.charts.application.useCases.ChartCreator;
+import org.example.core.charts.infrastructure.loaders.FileDataLoader;
+import org.example.core.charts.infrastructure.processors.BarChartProcessor;
+import org.example.core.movies.domain.Movie;
+import org.example.core.movies.infrastructure.CsvMovieSerializer;
+
+import java.util.Map;
+
+import static java.util.stream.Collectors.averagingInt;
+
 public class Main {
+    public static void main(String[] args) {
+        var creator = getChartCreator();
+    }
+
+    private static ChartCreator<Movie, Map<String, Double>> getChartCreator() {
+        var serializer = new CsvMovieSerializer();
+        var loader = new FileDataLoader<>("movies.csv", serializer);
+        var processor = new BarChartProcessor<>(Movie::genre, averagingInt(Movie::audienceScore));
+        return new ChartCreator<>(loader, processor);
+    }
 }
