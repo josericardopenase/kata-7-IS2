e7ef6fb68564c4609ea680dc1d9133d59a6860d7
josericardopenase <josericardopenase@gmail.com>
2024-01-20 07:31:11 +0100

    First commit

--- src/main/java/org/example/apps/desktop/Main.java
+++ src/main/java/org/example/apps/desktop/Main.java
@@ -5,6 +5,8 @@ import org.example.apps.desktop.infraestructure.swing.MainFrame;
 import org.example.core.charts.application.useCases.ChartCreator;
 import org.example.core.charts.infrastructure.loaders.FileDataLoader;
 import org.example.core.charts.infrastructure.processors.BarChartProcessor;
+import org.example.core.movies.domain.Movie;
+import org.example.core.movies.infrastructure.CsvMovieSerializer;
 import org.example.core.users.domain.User;
 import org.example.core.users.infrastructure.CsvUserSerializer;
 
@@ -21,10 +23,10 @@ public class Main {
         frame.setVisible(true);
     }
 
-    private static ChartCreator<User, Map<String, Double>> getChartCreator() {
-        var serializer = new CsvUserSerializer();
-        var loader = new FileDataLoader<>("users.csv", serializer);
-        var processor = new BarChartProcessor<>(User::firstName, averagingInt(x-> x.password().length()));
+    private static ChartCreator<Movie, Map<String, Double>> getChartCreator() {
+        var serializer = new CsvMovieSerializer();
+        var loader = new FileDataLoader<>("movies.csv", serializer);
+        var processor = new BarChartProcessor<>(Movie::genre, averagingInt(Movie::audienceScore));
         return new ChartCreator<>(loader, processor);
     }
 }
