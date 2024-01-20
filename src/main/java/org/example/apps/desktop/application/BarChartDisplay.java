697f24a45c0d27a40cf5ffa76e6b6ea1a1acc8ca
josericardopenase <josericardopenase@gmail.com>
2024-01-20 05:36:01 +0100

    Finished Application layer with BarChartScreen

---
+++ src/main/java/org/example/apps/desktop/application/BarChartDisplay.java
@@ -0,0 +1,7 @@
+package org.example.apps.desktop.application;
+
+import org.example.apps.desktop.domain.BarChart;
+
+public interface BarChartDisplay {
+    void display(BarChart barChart);
+}
---
+++ src/main/java/org/example/apps/desktop/application/BarChartScreen.java
@@ -0,0 +1,26 @@
+package org.example.apps.desktop.application;
+
+import org.example.apps.desktop.domain.BarChart;
+import org.example.core.charts.application.useCases.ChartCreator;
+import org.example.core.charts.domain.Chart;
+
+import java.util.Map;
+
+public class BarChartScreen<Model> implements Screen{
+    BarChartDisplay display;
+    ChartCreator<Model, Map <String, Double>> creator;
+    @Override
+    public void show() {
+        var chart = this.creator.create();
+        var barChart = chartToBarChart(chart);
+        display.display(barChart);
+    }
+
+    private BarChart chartToBarChart(Chart<Map<String, Double>> chart) {
+        return BarChart.fromScratch()
+                .setTitle("BarChart")
+                .setxAxisTitle("X axis")
+                .setyAxisTitle("Y axis")
+                .build();
+    }
+}
