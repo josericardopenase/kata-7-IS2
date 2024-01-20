0cc8c22113546905374d3ef65dcd46bf12a88cff
josericardopenase <josericardopenase@gmail.com>
2024-01-20 07:36:22 +0100

    Added connection in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,15 @@ import org.example.core.charts.infrastructure.processors.BarChartProcessor;
 import org.example.core.movies.domain.Movie;
 import org.example.core.movies.infrastructure.CsvMovieSerializer;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import static java.util.stream.Collectors.averagingInt;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        var connection = DriverManager.getConnection("jdbc:sqlite:moves.sqlite");
         var serializer = new CsvMovieSerializer();
         var loader = new FileDataLoader<>("movies.csv", serializer);
         var processor = new BarChartProcessor<>(Movie::genre, averagingInt(Movie::audienceScore));
