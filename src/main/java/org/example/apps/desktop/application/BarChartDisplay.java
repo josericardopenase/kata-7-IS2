0d983721ecb2268a5e1766cfa34a317aee691406
josericardopenase <josericardopenase@gmail.com>
2024-01-20 04:57:22 +0100

    Restarted project

--- src/main/java/org/example/apps/desktop/application/BarChartDisplay.java
+++
@@ -1,8 +0,0 @@
-package org.example.apps.desktop.application;
-
-
-import org.example.apps.desktop.domain.BarChart;
-
-public interface BarChartDisplay {
-    void display(BarChart barChart);
-}
--- src/main/java/org/example/apps/desktop/application/Screen.java
+++
@@ -1,5 +0,0 @@
-package org.example.apps.desktop.application;
-
-public interface Screen {
-    void show();
-}
