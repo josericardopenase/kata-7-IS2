de66df5cff092034c68b8e4a01ec435d9759de5d
josericardopenase <josericardopenase@gmail.com>
2024-01-20 07:33:37 +0100

    Implemented ModelSerializer with adapter pattern

--- src/main/java/org/example/core/charts/application/ports/ModelSerializer.java
+++ src/main/java/org/example/core/charts/application/ports/ModelSerializer.java
@@ -3,4 +3,9 @@ package org.example.core.charts.application.ports;
 public interface ModelSerializer <Model>{
     Model serialize(String data);
 
+    interface Input{
+        String get(String key);
+        String get(int index);
+    }
+
 }
